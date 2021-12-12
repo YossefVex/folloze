7dd9287f9a0c2cc26ff1ec5e7e5cc33ad259881d
yossefvex <7773899@gmail.com>
2021-12-12 21:40:46 +0200

    finish

--- try.js
+++ try.js
@@ -3,6 +3,8 @@ import LifecycleManager from './invoke.js'
 const func = ({a,b})=>{throw new Error('try error')}
 const func1 = ({a,b})=>{console.log(a+b)}
 const params = {a:1, b:2}
-
-const ins = new LifecycleManager({repeatTimes: 2});
+const beforeInvokingFunc = ()=>{console.log('BEFORE!!!')}
+const afterInvokingFunc = ()=>{console.log('AFTER!!!')}
+const ins = new LifecycleManager({repeatTimes: 2, beforeInvokingFunc, afterInvokingFunc});
 ins.invoke(func, params);
+// ins.invoke(func1, params);
